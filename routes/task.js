fd834aa9bac16b3a7db804bc1a97a33a1f41c996
xank <xank@qq.com>
2018-01-11 01:19:54 +0800

    完成统计项目

--- routes/task.js
+++ routes/task.js
@@ -4,8 +4,7 @@ const infoModel = require('../models/info')
 const moment = require('moment')
 const { openTimeToX, arrayAvg } = require('../lib/util')
 
-async function taskAtt(query, res, next) {
-  let { date = '', local = 'shanghai' } = query
+async function taskAtt(date, local = 'shanghai') {
   let data = await infoModel.getTaskAtt(local, date)
 
   for (let item of data) {
@@ -49,12 +48,21 @@ async function taskAtt(query, res, next) {
     // 更新数据
     await infoModel.update({ name, date, local }, { countWait })
   }
-  res.retData('countOk')
+  console.log(date, 'ok')
 }
 
 // 乐园项目日终统计
 router.get('/att', async (req, res, next) => {
-  await taskAtt(req.query, res, next)
+  let { date, local = 'shanghai', method } = req.query
+
+  // let st = 1492358400 // 20170417
+  // for (let day = 0; day <= 300; day++) {
+  //   let date = moment((st + 86400 * day) * 1000, 'x').format('YYYYMMDD')
+  //   await taskAtt(date, local)
+  // }
+
+  await taskAtt(date, local)
+  res.retData('countOk')
 })
 
 module.exports = router

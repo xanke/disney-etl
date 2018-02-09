e47bcc5f307b3f0de52fc7e73ae9127c75739540
xank <xank@qq.com>
2018-02-09 11:49:25 +0800

    add logs

--- util/logs.js
+++ util/logs.js
@@ -1,8 +1,17 @@
+const log4js = require('log4js')
+const logger = log4js.getLogger()
+
+log4js.configure({
+  appenders: { cheese: { type: 'file', filename: 'logs/etl.log' } },
+  categories: { default: { appenders: ['cheese'], level: 'all' } }
+})
+
 module.exports = {
   msg: (name, msg, conf) => {
     let { local, date } = conf
     let text = `${name} ${local} ${date} ${msg}`
     console.log(text)
+    logger.info(text)
     return text
   }
 }
